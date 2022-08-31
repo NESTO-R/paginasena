9621986dc2ead7066ed20174e2e0f3b0c1622e39
Usuario <davidcorrea48@outlook.com>
2022-08-31 13:50:16 -0500

    version3

--- menu.js
+++ menu.js
@@ -19,7 +19,21 @@ btnSwitch.addEventListener('click', () => {
      document.body.classList.toggle('dark');
      btnSwitch.classList.toggle('active');
 
+     //guarfamos el modo en localstorage.
+     if(document.body.classList.contains('dark')){
+          localStorage.setItem('dark-mode','true');
+     } else{
+          localStorage.setItem('dark-mode','false');
+     }
 });
+
+if(localStorage.getItem('dark-mode') === 'true'){
+     document.body.classList.add('dark');
+     btnSwitch.classList.toggle('active');
+} else {
+     document.body.classList.remove('dark');
+     btnSwitch.classList.remove('active');
+}
 
 (function(d){
      let tabs = Array.prototype.slice.apply(d.querySelectorAll('.tabs_items'));
